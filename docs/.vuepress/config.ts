ab0d2f19f90131a8a089d6cc250ef602178f52d3
whp98 <whp98@qq.com>
2021-12-10 15:57:06 +0800

    blog push

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,12 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: 'zh-CN',
+    title: 'HAIPENG_BLOG',
+    description: 'Just write it',
+    base: '/',
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+})
