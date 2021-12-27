47b77fb7d87511314e6aa2253e6f1a7d8fbcc4ac
whp98 <whp98@outlook.com>
2021-12-27 20:16:49 +0800

    发布博客，时间:12/27/2021 20:16:49

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,9 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
-
+import  path = require("path")
 export default defineUserConfig<DefaultThemeOptions>({
+    templateDev: path.join(__dirname, 'templates', 'index.dev.html'),
+	templateSSR: path.join(__dirname, 'templates', 'index.ssr.html'),
     lang: 'zh-CN',
     title: 'HAIPENG_BLOG',
     description: 'Just write it.',
