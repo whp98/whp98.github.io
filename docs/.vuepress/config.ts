2bbac554f90ce2f4f916788601c3421df3153e3b
whp98 <whp98@outlook.com>
2022-03-12 16:47:11 +0800

    发布博客，时间:2022-03-12 16:47:11

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,8 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
-import  path = require("path")
 export default defineUserConfig<DefaultThemeOptions>({
-/*     templateDev: path.join(__dirname, 'templates', 'index.dev.html'),
-	templateSSR: path.join(__dirname, 'templates', 'index.ssr.html'), */
     lang: 'zh-CN',
-    title: '🐖志',
+    title: '大猪Blog',
     description: '记录成长和生活',
     base: '/',
     themeConfig: {
