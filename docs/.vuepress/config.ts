80c8f6cfb55abe754fc11049f2161191f18f3cf7
whp98 <whp98@outlook.com>
2022-05-08 12:20:13 +0800

    发布博客，时间:2022-05-08 12:20:13

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 export default defineUserConfig<DefaultThemeOptions>({
     lang: 'zh-CN',
-    title: '大猪Blog',
+    title: '技术凡人',
     description: '记录成长和生活',
     base: '/',
     port: 34561,
