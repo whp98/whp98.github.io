a343c75dd04940a7e967d0c90131c42854434b8d
whp98 <whp98@outlook.com>
2022-11-19 23:30:13 +0800

    发布博客，时间:2022-11-19 23:30:13

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,6 +5,10 @@ export default {
     base: '/',
     port: 34561,
     themeConfig: {
-        logo: '/logo.png'
+        logo: '/logo.png',
+        footer: {
+            message: '基于<a href="https://github.com/whp98/whp98.github.io/blob/main/LICENSE.txt">MIT License</a>发布',
+            copyright: 'Copyright © 2019-present <a href="https://github.com/whp98">whp98</a> <br/> <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备19006659号-1</a>'
+        }
     }
 }
