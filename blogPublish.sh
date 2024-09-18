b04597f6aba137c48cbb08aa3613b2e3b06abbb9
whp98 <32877126+whp98@users.noreply.github.com>
2024-09-18 14:46:34 +0800

    发布博客，时间:2024/09/18 14:46:34

--- blogPublish.sh
+++ blogPublish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+nvm use 18
 echo `date +'%Y/%m/%d %H:%M:%S'`
 echo "开始发布";
 git fetch;pnpm run docs:build;git pull;pnpm run docs:build;git add .;pnpm run docs:build;git commit -m "发布博客，时间:`date +'%Y/%m/%d %H:%M:%S'`"; git push;
 echo "发布完成";
+nvm use 16
