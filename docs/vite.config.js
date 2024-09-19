4fafe3d6e087937e8f219ae3836fbc819ea24333
whp98 <32877126+whp98@users.noreply.github.com>
2024-09-19 20:45:35 +0800

    发布博客，时间:2024/09/19 20:45:35

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
     ],
     assetsInclude: ['**/*.PNG','**/*.JPG'],
     build: {
-        chunkSizeWarningLimit: 1024
+        chunkSizeWarningLimit: 10240
     }
 });
