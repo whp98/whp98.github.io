af3e20f3f47db22824ce62215f21ba40de6b2dbc
whp98 <32877126+whp98@users.noreply.github.com>
2024-09-19 16:43:09 +0800

    update code

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,6 +1,5 @@
 //vite.config.js
 import { defineConfig } from "vite";
-import { splitVendorChunkPlugin } from "vite";
 export default defineConfig({
     base: "./",
     server: {
@@ -9,7 +8,6 @@ export default defineConfig({
         disableHostCheck: true,
     },
     plugins: [
-        splitVendorChunkPlugin()
     ],
     assetsInclude: ['**/*.PNG','**/*.JPG']
 });
