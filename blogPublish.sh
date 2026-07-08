55994989bce109030a4f81a1cee0e17975983d02
whp98 <32877126+whp98@users.noreply.github.com>
2026-07-08 10:21:08 +0800

    发布博客，时间:2026/07/08 10:21:08

--- blogPublish.sh
+++ blogPublish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo `date +'%Y/%m/%d %H:%M:%S'`
 echo "开始发布";
-git fetch;fnm use 22;pnpm install;pnpm run docs:build;git pull;pnpm run docs:build;git add .;pnpm run docs:build;git commit -m "发布博客，时间:`date +'%Y/%m/%d %H:%M:%S'`"; git push;
+git fetch;fnm use 18;pnpm install;pnpm run docs:build;git pull;pnpm run docs:build;git add .;pnpm run docs:build;git commit -m "发布博客，时间:`date +'%Y/%m/%d %H:%M:%S'`"; git push;
 echo "发布完成";
